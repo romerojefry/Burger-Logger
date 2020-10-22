9c3a70f4edcea6517a195f5a6f8d53b8017bc446
Jefry Romero <romerojefry@icloud.com>
2020-10-22 16:36:04 -0700

    adding controllers set up and file

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,55 @@
+const express = require("express");
+
+const router = express.Router();
+
+const burgers = require("../models/burgers.js");
+
+router.get("/", function(req, res) {
+    burgers.all(function(data) {
+      var hbsObject = {
+        cats: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+  
+  router.post("/api/burgers", function(req, res) {
+    burgers.create([
+      "name", "sleepy"
+    ], [
+      req.body.name, req.body.sleepy
+    ], function(result) {
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burgers.update({
+      sleepy: req.body.sleepy
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
+  router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    burgers.delete(condition, function(result) {
+      if (result.affectedRows == 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
+  module.exports = router;
